2a56cb2b47426f29683526f58eb48b943ced57ab
Piscoi Andrei <andrei.piscoi@student.upt.ro>
2022-03-13 21:13:55 +0200

    Added username to thank you view

--- backend/Views/thank_you.py
+++ backend/Views/thank_you.py
@@ -2,8 +2,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from backend.Serializers.ticket_serializer import TicketSerializer
 from rest_framework.response import Response
+import json
 
 
 @api_view(['GET'])
-def thank_you(request, uid):
-    return Response("")
+def thank_you(request):
+    return Response(json.dumps({'name': request.user.username}))
