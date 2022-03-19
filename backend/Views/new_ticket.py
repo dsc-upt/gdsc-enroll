f6101ed5a99deccfde0c8f6f70de6fb18c0f222a
Piscoi Andrei <andrei.piscoi@student.upt.ro>
2022-03-19 14:21:01 +0200

    Removed unused request method

--- backend/Views/new_ticket.py
+++ backend/Views/new_ticket.py
@@ -4,14 +4,10 @@ from backend.Serializers.ticket_serializer import TicketSerializer
 from rest_framework.response import Response
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def new_ticket(request):
-    if request.method == 'POST':
-        serializer = TicketSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'GET':
-        return Response()
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    serializer = TicketSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
