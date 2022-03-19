796ffe93020e10000ed7a951d6093f40372779f0
Gaube Robert <robert-gabriel.gaube@student.upt.ro>
2022-03-19 15:02:49 +0200

    Adding APIView to new-ticket

--- backend/Views/new_ticket.py
+++ backend/Views/new_ticket.py
@@ -1,18 +1,11 @@
-from rest_framework import status
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView
 
 from backend.Serializers.ticket_serializer import TicketSerializer
-from rest_framework.response import Response
 
 
-@api_view(['POST'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
-def new_ticket(request):
-    serializer = TicketSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class NewTicket(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    serializer_class = TicketSerializer
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from backend.Views.rest_views import UserViewSet
 from rest_framework import routers
-from backend.Views.new_ticket import new_ticket
+from backend.Views.new_ticket import NewTicket
 from backend.Views.thank_you import thank_you
 
 
@@ -10,6 +10,6 @@ router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("new-ticket/", new_ticket),
+    path("new-ticket/", NewTicket.as_view()),
     path("thank-you/", thank_you)
 ]
