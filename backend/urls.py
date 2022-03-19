73425fff5204ad005819f7ac00a8e69e18f4c761
Piscoi Andrei <andrei.piscoi@student.upt.ro>
2022-03-19 14:11:33 +0200

    Removed useless url and view

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from backend.Views.rest_views import UserViewSet, GroupViewSet
 from rest_framework import routers
 from backend.Views.new_ticket import new_ticket
-from backend.Views.dashboard_view import dashboard
 from backend.Views.thank_you import thank_you
 
 
@@ -11,7 +10,6 @@ router.register(r"users", UserViewSet)
 router.register(r"groups", GroupViewSet)
 
 urlpatterns = [
-    path("dashboard/", dashboard),
     path("", include(router.urls)),
     path("new-ticket/", new_ticket),
     path("thank-you/", thank_you)
