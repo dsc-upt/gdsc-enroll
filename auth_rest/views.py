cadb8ac5bcd6bf584a4c3cf85b9532a6f42804f2
Piscoi Andrei <andrei.piscoi@student.upt.ro>
2022-03-26 15:13:54 +0200

    Update requirements

--- auth_rest/views.py
+++ auth_rest/views.py
@@ -1,15 +1,13 @@
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from django.urls import path
-from auth.views import MyObtainTokenPairView, RegisterView
-from rest_framework_simplejwt.views import TokenRefreshView
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
