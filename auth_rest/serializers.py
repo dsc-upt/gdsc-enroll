d4e1d5c9256de8b9861f05dd0266c8339077dd56
Gherasim Robert <ghera.robi@gmail.com>
2022-03-26 15:08:16 +0200

    Updated added serializer

--- auth_rest/serializers.py
+++ auth_rest/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
